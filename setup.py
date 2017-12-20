6ad080e4241c2c55d796bd279d4c0401a3ae9b98
Eric Chang <pkerichang@gmail.com>
2017-12-20 11:59:29 -0800

    adding setup files and directories.

---
+++ setup.py
@@ -0,0 +1,14 @@
+import os
+from distutils.core import setup, Extension
+from Cython.Build import cythonize
+
+setup(
+    ext_modules=cythonize(Extension('cyok',
+                                    sources=['src/cyok.pyx'],
+                                    language='c++',
+                                    include_dirs=['include'],
+                                    libraries=['okFrontPanel', 'dl'],
+                                    library_dirs=['lib'],
+                                    )
+    )
+)
